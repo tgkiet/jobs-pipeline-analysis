eb9b909b6da6203794869aa9759705bbc6965ce3
tgkiet <giakiettran14102005@gmail.com>
2025-05-28 23:05:26 +0700

    Revert "đã lưu được 50jobs từ trang 1 web topcv vào postgreSQL"
    This reverts commit a781abaa9b9ea6432c2481272c9a73f896b8147d.

---
+++ config.py
@@ -0,0 +1,7 @@
+DB_CONFIG = {
+    'dbname': 'my_database',
+    'user': 'postgres',
+    'password': 'MẬT_KHẨU_CỦA_USER_POSTGRES', # Cần nhập mật khẩu của user postgres
+    'host': 'localhost',
+    'port': '5432'
+}
